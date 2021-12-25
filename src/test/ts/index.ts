c008e71df9d1c1ace92d69e8284b0a0a4af3ed36
Anton Golub <antongolub@antongolub.com>
2021-12-25 18:27:00 +0300

    feat: add monorepo graph resolver `topo`

--- src/test/ts/index.ts
+++ src/test/ts/index.ts
@@ -1,8 +1,29 @@
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
+import { dirname, join, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-test('prepare, publish, addChannel are re-exported as is', () => {
-  assert.is(true, true)
+import { getManifestsPaths, topo } from '../../main/ts/index'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const fixtures = resolve(__dirname, '../fixtures')
+
+test('`getManifestsPaths` returns absolute package.json refs', async () => {
+  const cwd = resolve(fixtures, 'regular-monorepo')
+  const workspaces = ['packages/**/*/', '!packages/e']
+  const result = (await getManifestsPaths({cwd, workspaces})).sort()
+  const expected = ['a', 'c', 'd/d/d'].map(f => join(cwd, 'packages', f, 'package.json'))
+
+  assert.equal(result, expected)
+})
+
+test('`topo` returns monorepo release queue', async () => {
+  const cwd = resolve(fixtures, 'regular-monorepo')
+  const workspaces = ['packages/*']
+  const result = (await topo({cwd, workspaces})).queue
+  const expected = ['a', 'e', 'c']
+
+  assert.equal(result, expected)
 })
 
 test.run()
