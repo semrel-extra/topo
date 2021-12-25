4166a74684ea3c5319f8d5b51a386c7c5dc7fbce
Anton Golub <antongolub@antongolub.com>
2021-12-25 19:37:01 +0300

    build: configure es5 and es6 build

---
+++ src/test/js/index.cjs
@@ -0,0 +1,17 @@
+const {topo} = require('../../../target/es5/index.cjs')
+const {test} = require('uvu')
+const {resolve} = require('path')
+const assert = require('uvu/assert')
+
+const fixtures = resolve(__dirname, '../fixtures')
+
+test('`topo` returns monorepo release queue', async () => {
+  const cwd = resolve(fixtures, 'regular-monorepo')
+  const workspaces = ['packages/*']
+  const result = (await topo({cwd, workspaces})).queue
+  const expected = ['a', 'e', 'c']
+
+  assert.equal(result, expected)
+})
+
+test.run()
