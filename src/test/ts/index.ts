c581ed139b66c2696a30f593c0c06247deae1a5f
Anton Golub <antongolub@antongolub.com>
2021-12-27 21:17:22 +0300

    feat: add packages filter
    closes #6

--- src/test/ts/index.ts
+++ src/test/ts/index.ts
@@ -2,8 +2,9 @@ import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 import { dirname, join, resolve } from 'path'
 import { fileURLToPath } from 'url'
+import micromatch from 'micromatch'
 
-import { getManifestsPaths, topo } from '../../main/ts/index'
+import {getManifestsPaths, ITopoOptions, topo} from '../../main/ts/index'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const fixtures = resolve(__dirname, '../fixtures')
@@ -30,11 +31,48 @@ test('`topo` returns monorepo digest: release queue, deps graph, package manifes
     packages: {
       a: {
         manifest: {
-          name: 'a'
+          name: 'a',
+          private: true
         },
         manifestPath: join(cwd, 'packages/a/package.json'),
         path: 'packages/a'
       },
+      c: {
+        manifest: {
+          name: 'c',
+          dependencies: {
+            e: '*'
+          },
+        },
+        manifestPath: join(cwd, 'packages/c/package.json'),
+        path: 'packages/c'
+      },
+      e: {
+        manifest: {
+          name: 'e'
+        },
+        manifestPath: join(cwd, 'packages/e/package.json'),
+        path: 'packages/e'
+      }
+    }
+  }
+
+  assert.equal(result, expected)
+})
+
+test('`topo` applies filter', async () => {
+  const cwd = resolve(fixtures, 'regular-monorepo')
+  const workspaces = ['packages/*']
+  const filter: ITopoOptions['filter'] = ({path}) => !micromatch.isMatch(path, ['packages/a', 'packages/d/d'])
+  const result = await topo({cwd, workspaces, filter})
+
+  const expected = {
+    queue: ['e', 'c'],
+    nodes: ['c', 'e'],
+    edges: [
+      [ 'e', 'c' ]
+    ],
+    packages: {
       c: {
         manifest: {
           name: 'c',
